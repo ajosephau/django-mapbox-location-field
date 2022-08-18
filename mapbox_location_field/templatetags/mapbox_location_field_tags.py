77c84e2ef37e5f7e570409dcbc2938d5005111c2
Anthony Joseph <ajosephau@users.noreply.github.com>
2022-08-18 10:13:12 +1000

    Update mapbox_location_field_tags.py

--- mapbox_location_field/templatetags/mapbox_location_field_tags.py
+++ mapbox_location_field/templatetags/mapbox_location_field_tags.py
@@ -6,14 +6,13 @@ register = template.Library()
 
 @register.simple_tag
 def location_field_includes():
-    html = "<meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no'/><script src='https://api.tiles.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.js'></script><link href='https://api.tiles.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.css' rel='stylesheet'/><script src='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.0/mapbox-gl-geocoder.min.js'></script><link rel='stylesheet' href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.0/mapbox-gl-geocoder.css' type='text/css'/>"
+    html = "<meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no'/><script src='https://static.bikeparty.com.au/static/libraries/mapbox_gl/mapbox-gl.js'></script><link href='https://static.bikeparty.com.au/static/libraries/mapbox_gl/mapbox-gl.css' rel='stylesheet'/><script src='https://static.bikeparty.com.au/static/libraries/mapbox_gl_geocoder/mapbox-gl-geocoder.min.js'></script><link rel='stylesheet' href='https://static.bikeparty.com.au/static/libraries/mapbox_gl_geocoder/mapbox-gl-geocoder.css' type='text/css'/>"
     return mark_safe(html)
 
 
 @register.simple_tag
 def include_jquery():
-    return mark_safe(
-        "<script src='https://code.jquery.com/jquery-3.3.1.min.js' integrity='sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT' crossorigin='anonymous' ></script>")
+    return mark_safe("")
 
 
 @register.filter
